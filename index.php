c0c33641441d3226989f096f611902022a83eb74
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-05-01 18:50:50 -0400

    Agregar Empleado al CRUD con PHP

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 require_once "Controladores/rutasC.php"; // Accediendo a rutas del controlador
 require_once "Controladores/adminC.php"; // Accediendo a controlador de admin
+require_once "Controladores/empleadosC.php"; // Accediendo a controlador de empleados
 
 require_once "Modelos/rutasM.php";// Accediendo a rutas del modelo
 require_once "Modelos/adminM.php"; // Accediendo a modelo de admin
+require_once "Modelos/empleadosM.php"; // Accediendo a modelo de empleados
 
 $rutas = new RutasControlador(); // Instanciando la clase
 $rutas -> Plantilla(); // Accediendo a la plantilla
